420febf51d20dc988440873f4e501a1302b4b366
Gytis <gytis@reservationpartner.lt>
2014-08-05 09:45:21 +0300

    Updated functions and descriptions

--- classes/MyTools.php
+++ classes/MyTools.php
@@ -2,9 +2,11 @@
 
 class MyTools extends Tools
 {
-
-    /*
-     * Returns an array of enabled language IDs for iterations
+    /**
+     * Returns shop's languages IDs array
+     * @param bool $active
+     * @param bool $id_shop
+     * @return array Integer array of IDs
      */
     public static function getLangIDs($active = false, $id_shop = false){
         $langIDs = array();
@@ -14,23 +16,45 @@ class MyTools extends Tools
         return $langIDs;
     }
 
-    /*
-     * Makes a language array, keys are language IDs, values are the same ($value)
+    /**
+     * Returns shop's users groups IDs array
+     * @return array Integer array of IDs
      */
-    public static function makeValueLangArray($value = ''){
+    public function getGroupsIDs(){
+        $sql = new DbQuery();
+        $sql->select('id_group')->from('group');
+        $rows = Db::getInstance()->executeS($sql);
+        $ids = array();
+        foreach($rows as $row){
+            $ids[] = (int) $row['id_group'];
+        }
+        return $ids;
+    }
+
+    /**
+     * Makes a language array, keys are language IDs, values are the same ($value)
+     * @param mixed $value
+     * @return array
+     */
+    public static function makeValueLangArray($value = null){
         return array_fill_keys( self::getLangIDs(), $value );
     }
 
-    /*
-     * Parses submitted floating point number and returns it
+    /**
+     * Parses submitted floating point number (replaces commas and convert to float) and returns it
+     * @param $key
+     * @param bool $default_value
+     * @return float
      */
     public static function getValueFloat($key, $default_value = false){
         return (float) str_replace(',', '.', Tools::getValue($key, $default_value));
     }
 
-    /*
-     * Execute an array of SQL statements
-    */
+    /**
+     * Executes an array of SQL statements
+     * @param array $sql
+     * @return bool
+     */
     public static function runSQL($sql)
     {
         $db = Db::getInstance();
@@ -48,62 +72,86 @@ class MyTools extends Tools
         return true;
     }
 
-    public static function emailLink( $email ){
+    /**
+     * Creates an HTML email link
+     * @param string $email
+     * @return string HTML
+     */
+    public static function emailLink($email){
         return '<a href="mailto:'.$email.'">'.$email.'</a>';
     }
 
-    public static function telephoneLink( $phoneNum ){
+    /**
+     * Creates an HTML phone link (works on Android, Skype)
+     * @param string $phoneNum
+     * @return string HTML
+     */
+    public static function telephoneLink($phoneNum){
         return '<a href="tel:'.$phoneNum.'">'.$phoneNum.'</a>';
     }
 
-    public static function renderFormView($fields, $titles = array() ){
-
-        $fields_form[0]['form'] = array();
-        foreach($fields as $fieldKey => $fieldVal){
-            $fields_form[0]['form']['input'][] = array(
-                'type'  => 'free',
-                'label' => $titles[$fieldKey],
-                'name'  => $fieldKey,
-            );
-        }
-
-        $helper = new HelperForm();
-        $helper->fields_value = $fields;
-        $helper->show_toolbar = false;
-
-        $view = $helper->generateForm($fields_form);
-        $view = self::removeScript($view);
-        $view = self::removeFormTags($view);
-
-        $style = '';
-        if(!self::isBootstrap()){
-            $style = '<style>.margin-form{padding-top:0.3em;font-size:0.9em;word-break:break-all;</style>';
-        }
-
-        return $style.$view;
-    }
-
+    /**
+     * Removes <script>.*</script> blocks from HTML
+     * @param $html HTML
+     * @return string HTML
+     */
     public static function removeScript( $html ){
         return trim( preg_replace('#\<script[.\s\S]*?\<\/script\>#mi', '', $html) );
     }
 
+    /**
+     * Removes only <form>, </form> tags, not the content
+     * @param $html HTML
+     * @return string HTML
+     */
     public static function removeFormTags( $html ){
         return trim( preg_replace('#<\/?form.*?>#mi', '', $html) );
     }
 
+    /**
+     * Checks if PrestaShop is using bootstrap
+     * @return bool
+     */
     public static function isBootstrap(){
         return (version_compare(_PS_VERSION_, 1.6) >= 0) ? true : false;
     }
 
+    /*
+     *
+     */
+    /**
+     * Creates a publicly readable path (folders)
+     * @param $fullPath Full local folder path
+     * @return bool
+     */
     public static function makePublicDir( $fullPath ){
 
+        $isRecursive = true;
         $isCreateDir = true;
         if(!is_dir($fullPath)){
-            $isCreateDir = @mkdir($fullPath, 0775);
+            $isCreateDir = mkdir($fullPath, 0775, $isRecursive);
         }
-        @chmod($fullPath, 0775);
+        chmod($fullPath, 0775);
 
         return $isCreateDir;
+    }
+
+    /**
+     * @param  string $type Can be 'error', 'success', 'warning'
+     * @param  string $text Text to be placed inside the message
+     * @param  bool $time Appends time text to the message
+     * @return string Message HTML
+     */
+    public function msg($type, $text, $checked = false){
+        $isBootstrap = self::isBootstrap();
+        $classes = array(
+            'error'   => $isBootstrap ? 'alert alert-danger'  : 'error',
+            'success' => $isBootstrap ? 'alert alert-success' : 'conf confirm',
+            'warning' => $isBootstrap ? 'alert alert-warning' : 'warn warning',
+        );
+        $class = array_key_exists($type, $classes) ? $classes[$type] : $classes['success'];
+        $timeText = $checked ? sprintf($this->l('Checked @ %s'), date('Y-m-d H:i:s')) : '';
+        return '<div class="'.$class.'">'.$text.' '.$timeText.'.</div>';
     }
 
 }
