35625cfd023813e5132248a0bc1ba0313b2038b2
Gytis <gytis@reservationpartner.lt>
2014-08-05 12:11:47 +0300

    Renamed file

--- controllers/admin/MyRedirectConfigureController.php
+++ controllers/admin/MyRedirectConfigureController.php
@@ -1,12 +1,13 @@
 <?php
 
-class AdminRequestAModuleConfigurationController extends AdminController
+class AdminMyModuleRedirectConfigureController extends AdminController
 {
     public function __construct()
     {
         parent::__construct();
         $href = $this->context->link->getAdminLink('AdminModules')
-            .'&configure=requestamodule&token='.Tools::getAdminTokenLite('AdminModules');
+            .'&configure=mymodule&token='.Tools::getAdminTokenLite('AdminModules');
+
         Tools::redirectAdmin($href);
     }
 }
